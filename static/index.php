b2de8cc848d60f7938c1bbba726ad5270439242a
unknown <guillaume.scherrer@isen.fr>
2015-02-05 11:37:51 +0100

    dossier statique= prmiere version de la page avec formulaire de contacte.  Back end à faire et logo à centrer
    Signed-off-by: unknown <guillaume.scherrer@isen.fr>

---
+++ static/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chupil</title>
+	<link rel="icon" href="img/logo_16px.ico" />
+	<meta charset='utf-8'>
+	<style>
+		
+	html{
+			background-color: #352e2c;
+			margin: 0;
+		}
+		
+		/*#logo { 
+			width: 828px;
+			height: 243px;
+			margin: auto; 
+			position: fixed;
+			top: 50%;
+			margin-top: 0px;
+			left: 50%;
+			margin-left: -414px;		 	
+		}*/
+		.container{
+			margin-top: 243px;
+		}
+		#top{
+			background-color: #352e2c;
+			/*height: 25%;
+			width: 12%;*/
+		}
+		h1.text-center{
+			color: white;
+		}
+
+	</style>
+	 <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+        <meta charset="utf-8">
+        <title>Chupil</title>
+        <meta name="generator" content="Onasus.com" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link href="../lib/bootstrap-3.3.2-dist/css/bootstrap.css" rel="stylesheet">
+</head>
+<body  >
+	<div class="container">
+		<div class="row">
+			<div id="top" class="col-lm-8"> <img src="img/logo_min.png"></div>
+		</div>
+	</div>
+    <div class="container" >   
+        <div class="row">     
+			<h1 class="text-center" >Formulaire de contact</h1>
+			   
+			<hr>    
+			<div class="col-sm-8">
+			<form role="form">
+				<div class="row form-group">
+					<div class="col-xs-3">
+						<input class="form-control" id="firstName" name="firstName" placeholder="First Name" required="" type="text">
+					</div>            
+					<div class="col-xs-3">                
+						<input class="form-control" id="middleName" name="firstName" placeholder="Middle Name" required="" type="text">            
+					</div>
+					<div class="col-xs-4">
+						<input class="form-control" id="lastName" name="lastName" placeholder="Last Name" required="" type="text">            
+					</div>        
+				</div>        
+				<div class="row form-group">            
+					<div class="col-xs-5">                
+						<input class="form-control" name="email" placeholder="Email" required="" type="email">
+					</div>
+					<div class="col-xs-5">                
+						<input class="form-control" name="phone" placeholder="Phone" required="" type="email">
+					</div>        
+				</div>        
+				<div class="row form-group">            
+					<div class="col-xs-10">                
+						<input class="form-control" placeholder="Website URL" required="" type="homepage">
+					</div>        
+				</div>
+				
+			</form>
+			</div>    
+			<div class="col-sm-3 pull-right"> 
+				<div class="row form-group">            
+					<div class="col-xs-10">                
+						<textarea class="form-control" rows="5">Your message here</textarea>
+					</div>        
+				</div>
+				        
+				<div class="row form-group">            
+					<div class="col-xs-10">                
+						<button class="btn btn-default pull-right">Envoyer</button>
+					</div>        
+				</div>     
+			</div>
+		</div>
+    </div > 
+        <script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+
+        <script type='text/javascript' src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+        
+    </body>
+</html>
