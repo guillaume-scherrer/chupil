fcc8e58f7de9061514431c508301031c742a9b44
unknown <guillaume.scherrer@isen.fr>
2015-02-06 11:04:18 +0100

    Static+mailing finis.

--- static/index.php
+++ static/index.php
@@ -5,59 +5,32 @@
 	<link rel="icon" href="img/logo_16px.ico" />
 	<meta charset='utf-8'>
 	<style>
-		
-		html{
+		html,body{
 			background-color: #352e2c;
 			margin: 0;
-			overflow-x: hidden;
 		}
-		
-		
-		
-		#top{
-			background-color: #352e2c;
+		div{
 			width: 828px;
 			height: 243px;
 			margin: auto; 
-			position: relative;
-			top: 50px;
+			position: fixed;
+			top: 50%;
+			margin-top: -175px;
+			left: 50%;
+			margin-left: -414px;		
 		}
-		
-		h1.text-center{
-			color: white;
-		}
-
-		.rowd{
-			position: absolute;
-			margin: auto;
-			width: 828px;
-			height: 243px;
-		}
-		.row div{
-			display: block;
+		#logo { 
 			
 		}
+		div span{
+			width: 50px;
+			display: block;
+			margin: auto;
+			font-family: 'Open Sans', Arial, sans-serif;
+			color: white;
+			font-size: 8px;
+		}
 		
-		#form{
-			position: relative;
-			top: 150px;
-			margin: auto;
-			width: 640px;
-			min-height: 300px;
-			height: auto;
-		}
-		.row{
-			margin: 0;
-			padding: 0;
-		}
-		*{
-			margin: 0;
-			padding: 0;
-		}
-		form div{
-			margin: auto;
-			width: 800px;
-		}
 		#loader{
 			position: absolute;
 			bottom: 10px;
@@ -70,66 +43,11 @@
 		    transform: rotate(360deg);
 		}
 	</style>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
-        <meta charset="utf-8">
-        <title>Chupil</title>
-        <meta name="generator" content="Onasus.com" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <link href="../lib/bootstrap-3.3.2-dist/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
-	<div id="rowd" class="containerd">
-		<div class="row">
-			<div id="top" class="col-lm-8"> 
-		<img id="loader" src="img/load.png">
-
-				<img id="logo" src="img/logo_min.png">
-			</div>		
-		</div>
-	</div>
 	<div>
- 	<form id='form' role="form">
-				<div class="row form-group">
-					<div class="col-xs-3">
-						<input class="form-control" id="firstName" name="firstName" placeholder="First Name" required="" type="text">
-					</div>            
-					<div class="col-xs-3">                
-						<input class="form-control" id="middleName" name="firstName" placeholder="Middle Name" required="" type="text">            
-					</div>
-					<div class="col-xs-4">
-						<input class="form-control" id="lastName" name="lastName" placeholder="Last Name" required="" type="text">            
-					</div>        
-				</div>        
-				<div class="row form-group">            
-					<div class="col-xs-5">                
-						<input class="form-control" name="email" placeholder="Email" required="" type="email">
-					</div>
-					<div class="col-xs-5">                
-						<input class="form-control" name="phone" placeholder="Phone" required="" type="email">
-					</div>        
-				</div>        
-				<div class="row form-group">            
-					<div class="col-xs-10">                
-						<input class="form-control" placeholder="Website URL" required="" type="homepage">
-					</div>        
-				</div>
-				<div class="row form-group">            
-					<div class="col-xs-10">                
-						<textarea class="form-control" rows="5">Your message here</textarea>
-					</div>        
-				</div>
-				        
-				<div class="row form-group">            
-					<div class="col-xs-10">                
-						<button class="btn btn-default pull-right">Envoyer</button>
-					</div>        
-				</div>
-			</form>
-	</div>		
-       <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-
-
-        <script type='text/javascript' src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-        
-    </body>
+		<img id="loader" src="img/load.png">
+		<img id="logo" src="img/logo_min.png" >
+	</div>
+</body>
 </html>
